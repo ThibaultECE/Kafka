55cdf21f4b978ddb68aa93bfe71c3c8f484ba928
ThibaultECE <thibault.peeters@live.fr>
2024-04-09 15:17:35 +0200

    v0.1 stable

--- argo.go
+++ argo.go
@@ -2,54 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"github.com/otiai10/gosseract/v2"
-	"io/ioutil"
 	"os"
     "os/exec"
-    "path/filepath"
 )
 
-func DemoConvertir(chemin string) ([]string, error) {
-	tempDir,err := ioutil.TempDir("", "video_frames")
+func CreerFichiersImage(inputFile string, outputDir string, segmentDuration int) error {
+
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+	 return fmt.Errorf("failed to create output directory: %v", err)
+	}
+
+	ffmpegCmd := exec.Command(
+		"ffmpeg", 
+		"-i",
+		inputFile, 
+		"-vf",
+		"fps=1/5",
+		"temp/image%d.png",
+	)
+   
+	output, err := ffmpegCmd.CombinedOutput()
 	if err != nil {
-        return nil, err
-    }
-	defer os.RemoveAll(tempDir)
+	 return fmt.Errorf("failed to create HLS: %v\nOutput: %s", err, string(output))
+	}
+   
+	return nil
+   }
 
-	imagePattern := filepath.Join(tempDir, fmt.Sprintf("image-%03d.jpg",100))
-	fmt.Printf(fmt.Sprintf("image-%03d.jpg",100))
-	cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1/1", imagePattern)
-	cmd.Run()
-	return nil, nil
-}
-
-func ConvertirVideoEnImage(chemin string) ([]string, error) {
-
-	// tempDir,err := ioutil.TempDir("", "video_frames")
-	// if err != nil {
-    //     return nil, err
-    // }
-	tempDir := "/temp"
-	// defer os.RemoveAll(tempDir)
-
-	imagePattern := filepath.Join(tempDir, fmt.Sprintf("%03d.jpg",100))
-	fmt.Printf("ffmpeg", "-i", chemin, "-vf", "fps=1 %03d.jpg")
-	cmd := exec.Command("ffmpeg -i "+chemin + " fps=1 %03d.jpg")
-	// cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1", imagePattern)
-	// cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1 %03d.jpg")
-
-	err := cmd.Run()
-    if err != nil {
-		fmt.Printf("erreur dans l exec de la commande")
-        return nil, err
-    }
-	fmt.Printf("Début de la traduction\n")
+func Traducteur(chemin string,frames int) ([]string, error) {
+	
+	fmt.Printf("\nDébut de la traduction")
 	client := gosseract.NewClient()
 	defer client.Close()
 	client.SetLanguage("fra")
 
+	imagePattern := "temp/image%d.png"
+
 	var livreBrut []string
-	for i := 1; ; i ++ {
+	for i := 1; i <= frames ; i ++ {
 		cheminImage := fmt.Sprintf(imagePattern,i)
         _, err := os.Stat(chemin)
         if os.IsNotExist(err) {
@@ -83,27 +75,52 @@ func extractTextFromImage(client *gosseract.Client, cheminImage string) (string,
 }
 
 func main() {
+	inputFile := "sample.mp4"
+	outputDir := "temp/"
+	segmentDuration := 10 
+   
+	if err := CreerFichiersImage(inputFile, outputDir, segmentDuration); err != nil {
+	 log.Fatalf("Erreur dans le découpage: %v", err)
+	}
+   
+	log.Println("\nSuccès du découpage")
 
-	cheminVideo := "sample.mp4"
-	transcription, err := ConvertirVideoEnImage(cheminVideo)
-	// transcription, err := DemoConvertir(cheminVideo)
+	transcription, err := Traducteur(inputFile,4)
 	if err != nil {
-		fmt.Printf("Une erreur est survenue: ", err)
+		fmt.Printf("\nUne erreur est survenue: ", err)
 		return
 	}
-	fmt.Printf("Le traitement c'est bien passé\n")
+	fmt.Printf("\nLe traitement c'est bien passé")
 	for i, text := range transcription	{
         fmt.Printf("Texte extrait de l'image %d:\n%s\n", i+1, text)	
 	}
 
+	os.RemoveAll("temp")
+	os.MkdirAll("temp/",0750)
+   }
 
-	// client := gosseract.NewClient()
-	// defer client.Close()
-	// client.SetLanguage("fra")
-	// // client.SetImage("001-helloworld.png")
-	// client.SetImage("sample.jpg")
-	// text, _ := client.Text()
-	// fmt.Println(text)
-	// fmt.Println("text")
-	// // Hello, World!
-}
+// func main() {
+
+// 	cheminVideo := "sample.mp4"
+// 	transcription, err := ConvertirVideoEnImage(cheminVideo)
+// 	// transcription, err := DemoConvertir(cheminVideo)
+// 	if err != nil {
+// 		fmt.Printf("Une erreur est survenue: ", err)
+// 		return
+// 	}
+// 	fmt.Printf("Le traitement c'est bien passé\n")
+// 	for i, text := range transcription	{
+//         fmt.Printf("Texte extrait de l'image %d:\n%s\n", i+1, text)	
+// 	}
+
+
+// 	// client := gosseract.NewClient()
+// 	// defer client.Close()
+// 	// client.SetLanguage("fra")
+// 	// // client.SetImage("001-helloworld.png")
+// 	// client.SetImage("sample.jpg")
+// 	// text, _ := client.Text()
+// 	// fmt.Println(text)
+// 	// fmt.Println("text")
+// 	// // Hello, World!
+// }
