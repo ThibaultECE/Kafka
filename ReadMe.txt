64a2cc26acc5b5c0121424c301250577c1624c37
thibaultECE <thibault.peeters@live.fr>
2024-04-09 02:14:23 +0200

    poste

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,6 @@ sudo apt install libtesseract-dev
 Pour installer une lib C
 
 https://github.com/otiai10/gosseract/blob/main/Dockerfile
+
+Command a reproduire
+ffmpeg -i sample.mp4 -vf fps=1 %03d.png
--- argo.go
+++ argo.go
@@ -7,42 +7,76 @@ import (
 	"os"
     "os/exec"
     "path/filepath"
-    "strings"
 )
+
+func DemoConvertir(chemin string) ([]string, error) {
+	tempDir,err := ioutil.TempDir("", "video_frames")
+	if err != nil {
+        return nil, err
+    }
+	defer os.RemoveAll(tempDir)
+
+	imagePattern := filepath.Join(tempDir, fmt.Sprintf("image-%03d.jpg",100))
+	fmt.Printf(fmt.Sprintf("image-%03d.jpg",100))
+	cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1/1", imagePattern)
+	cmd.Run()
+	return nil, nil
+}
 
 func ConvertirVideoEnImage(chemin string) ([]string, error) {
 
-	tempDir := ioutil.TempDir("", "video_frames")
-	defer os.RemoveAll(tempDir)
+	// tempDir,err := ioutil.TempDir("", "video_frames")
+	// if err != nil {
+    //     return nil, err
+    // }
+	tempDir := "/temp"
+	// defer os.RemoveAll(tempDir)
 
-	imagePattern := filepath.Join(tempDir, "image-%03d.jpg")
-	cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1/1", imagePattern)
+	imagePattern := filepath.Join(tempDir, fmt.Sprintf("%03d.jpg",100))
+	fmt.Printf("ffmpeg", "-i", chemin, "-vf", "fps=1 %03d.jpg")
+	cmd := exec.Command("ffmpeg -i "+chemin + " fps=1 %03d.jpg")
+	// cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1", imagePattern)
+	// cmd := exec.Command("ffmpeg", "-i", chemin, "-vf", "fps=1 %03d.jpg")
 
-	err = cmd.Run()
+	err := cmd.Run()
     if err != nil {
+		fmt.Printf("erreur dans l exec de la commande")
         return nil, err
     }
-
+	fmt.Printf("Début de la traduction\n")
 	client := gosseract.NewClient()
 	defer client.Close()
 	client.SetLanguage("fra")
 
-	var livres []string
+	var livreBrut []string
 	for i := 1; ; i ++ {
 		cheminImage := fmt.Sprintf(imagePattern,i)
-		alpha := os.Stat(cheminImage)
+        _, err := os.Stat(chemin)
+        if os.IsNotExist(err) {
+            break 
+        }
 
-		textBrut := extractTextFromImage(client,cheminImage)
+		textBrut, err := extractTextFromImage(client,cheminImage)
+		if err != nil {
+            return nil, err
+        }
 		livreBrut = append(livreBrut,textBrut)
 	}
 
-	return texts, nil
+	return livreBrut, nil
 }
 
 func extractTextFromImage(client *gosseract.Client, cheminImage string) (string, error) {
 
-	client.SetImage(cheminImage)
-	page := client.Text()
+	err := client.SetImage(cheminImage)
+    if err != nil {
+        return "", err
+    }
+
+	page,err := client.Text()
+	if err != nil {
+        return "", err
+    }
 
 	return page, nil
 
@@ -52,11 +86,12 @@ func main() {
 
 	cheminVideo := "sample.mp4"
 	transcription, err := ConvertirVideoEnImage(cheminVideo)
+	// transcription, err := DemoConvertir(cheminVideo)
 	if err != nil {
 		fmt.Printf("Une erreur est survenue: ", err)
 		return
 	}
-
+	fmt.Printf("Le traitement c'est bien passé\n")
 	for i, text := range transcription	{
         fmt.Printf("Texte extrait de l'image %d:\n%s\n", i+1, text)	
 	}
